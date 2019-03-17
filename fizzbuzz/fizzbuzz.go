f17d0e7f98b65e5db1ad233084d965473d1d6644
Nico <nicovigil1@gmail.com>
2019-03-16 23:50:53 -0600

    adds fizzbuzz with commandline argument

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,28 @@
+package main
+
+import ("os"
+				"strconv"
+        "fmt")
+// test := []int
+
+func main() {
+	for _, j := range buildArray() { 
+		switch {
+		case j % 3 == 0:
+			fmt.Println("fizz")
+		case j % 5 == 0:
+			fmt.Println("buzz")
+		default:
+			fmt.Println(j) 
+		}
+	}
+}
+
+func buildArray() []int {
+	length, _ := strconv.Atoi(os.Args[1])
+	test_slice := make([]int, length)
+	for i := 0; i <= length-1; i++ {
+		test_slice[i] += i + 1
+	}
+	return test_slice
+}
